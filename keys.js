6f728402b0404d86bf0005ef5d9efab2db6d2034
k-lord <katielord27@gmail.com>
2019-11-14 12:32:13 -0500

    added dotenv package, created keys.js with export logic, and created .env file with actual API data

---
+++ keys.js
@@ -0,0 +1,17 @@
+console.log('keys.js is loaded!');
+
+//Spotify API Keys:
+exports.spotify = {
+  id: process.env.SPOTIFY_ID,
+  secret: process.env.SPOTIFY_SECRET
+};
+
+//Bands In Town API Key:
+exports.bandsInTown = {
+    id: process.env.BANDS_ID
+};
+
+//OMDB API Key:
+exports.omdb = {
+    id: process.env.OMDB_ID
+};
