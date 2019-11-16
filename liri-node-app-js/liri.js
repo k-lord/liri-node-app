610799be23e5f95bb703c7fda2d7bd03a9fb87ee
k-lord <katielord27@gmail.com>
2019-11-15 20:46:48 -0500

    cleaning up before video filming

--- liri-node-app-js/liri.js
+++ liri-node-app-js/liri.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
-var keys = require("./keys.js");
+var keys = require("./keys.js.js");
 var fs = require("fs");
 var axios = require("axios");
 var moment = require("moment");
